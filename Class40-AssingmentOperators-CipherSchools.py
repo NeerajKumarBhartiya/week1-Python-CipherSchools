6602b7d4b69b809602ea1f1ff97bf3504a304421
Neeraj Kumar <106676463+NeerajKumarBhartiya@users.noreply.github.com>
2022-12-01 23:25:37 +0530

    Add files via upload

---
+++ Class40-AssingmentOperators-CipherSchools.py
@@ -0,0 +1,6 @@
+name= "Neeraj"
+name += "it"
+print(name)
+age=19
+age-=1
+print(age)
---
+++ Class51-Keyword-CipherSchools.py
@@ -0,0 +1,7 @@
+name = "Neeraj"
+# in keyword
+# if with in
+if 'N' in "Neeraj":
+    print("N is present in name")
+else:
+    print("not present")
